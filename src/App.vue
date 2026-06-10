ba43d9d6fe9a15246946bb37c57ae8b00e448f0b
Bruno <b-d.dupuis@laposte.net>
2026-06-10 18:16:06 +0200

    first

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+import content from '../content/home.json'
+</script>
+
+<template>
+  <h1>{{ content.titre }}</h1>
+  <div>
+    <p>
+      {{ content.paragraph }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: blueviolet;
+  text-align: center;
+}
+
+p {
+  text-align: center;
+}
+
+div {
+  margin: 0 auto;
+  border: 2px red solid;
+  width: 500px;
+}
+</style>
